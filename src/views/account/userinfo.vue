3f64a3d60946db6f6339b6b918ed980e3f79455a
woxiangchixingxing <wi98590@gmail.com>
2019-04-27 21:27:55 +0800

    新增用户信息页面布局

---
+++ src/views/account/userinfo.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="title">个人信息</div>
+    <van-cell title="头像">
+      <van-icon slot="right-icon" name="search" class="custom-icon" />
+    </van-cell>
+    <van-cell title="昵称" value="" is-link />
+    <van-cell title="手机号" value="" />
+    <van-cell title="性别" value="" is-link />
+    <van-cell title="城市" value="" is-link/>
+    <van-cell title="职业状态" value="" is-link />
+    <van-dialog
+      v-model="showSetNickname"
+      show-cancel-button
+    >
+      <input type="text" placeholder="输入昵称" maxlength="10">
+    </van-dialog>
+    <van-actionsheet
+      v-model="showSetGender"
+      :actions="[{name: '女'},{name: '男'},{name: '保密'}]"
+      cancel-text="取消"
+      @select="onSelect"
+      @cancel="onCancel"
+    />
+    <van-popup v-model="showSetCity" position="bottom" :overlay="true">
+      <van-area :area-list="areaList" :columns-num="2" title="标题" />
+    </van-popup>
+    <van-actionsheet
+      v-model="showSetIdentity"
+      :actions="[{name: '学生党'},{name: '上班族'},{name: '其他'}]"
+      cancel-text="取消"
+      @select="onSelect"
+      @cancel="onCancel"
+    />
+  </div>
+</template>
+<script>
+import area from '../../lib/area.js'
+export default {
+  data: function() {
+    return {
+      showSetNickname: false,
+      showSetGender: false,
+      showSetCity: false,
+      showSetIdentity: true,
+      areaList: area
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.title {
+  padding: 15px 20px;
+}
+input {
+  box-sizing: border-box;
+  margin: 50px 0px;
+  text-align: center;
+  width: 100%;
+  border: 0px;
+}
+</style>
