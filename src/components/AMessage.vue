b1f278718296449fa983097df54b4d2984c5ef35
woxiangchixingxing <wi98590@gmail.com>
2019-05-23 20:48:50 +0800

    add AMessage page

--- src/components/AMessage.vue
+++ src/components/AMessage.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>a message</div>
+  <div>
+    <div class="msg l-msg">
+      <img src="/default_avatar.png" alt="" class="avatar">
+      <div class="content">
+        <div>
+          <div class="time">11:20</div>
+        </div>
+        <div>
+          <div class="text">123,446.123446.12446.12446.12446.12446.12446.12446.12446.12446.12</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -7,5 +19,60 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+.msg {
+  display: flex;
+  width: 100%;
+  margin-bottom: 20px;
+}
+.l-msg {
+  .avatar {
+    margin-right: 10px;
+  }
+  .content {
+    .text {
+      border-top-left-radius: 0px;
+    }
+  }
+}
+.r-msg {
+  flex-direction: row-reverse;
+  .avatar {
+    margin-left: 10px;
+  }
+  .content {
+    text-align: right;
+    .text {
+      border-top-right-radius: 0px;
+    }
+  }
+}
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 6px;
+}
+.content {
+  width: calc(100% - 100px);
+  .time {
+    display: inline-block;
+    font-size: 12px;
+    background-color: #aaa;
+    color: #fff;
+    padding: 2px 4px;
+    border-radius: 12px;
+  }
+  .text {
+    text-align: left;
+    box-sizing: border-box;
+    border-radius: 15px;
+    max-width: 100%;
+    display: inline-block;
+    background-color: #fff;
+    // box-shadow: 0px 0px 2px rgba(0,0,0,0.08);
+    border: 1px solid #efefef;
+    margin-top: 10px;
+    padding: 13px;
+    word-wrap:break-word;
+  }
+}
 </style>
--- src/views/message/chat.vue
+++ src/views/message/chat.vue
@@ -3,7 +3,13 @@
     <div class="log-area">
       <a-message />
     </div>
-    <div class="control-area">123</div>
+    <div class="control-area">
+      <div class="transaction">发起易书</div>
+      <div class="msg">
+        <input class="input" type="text" name="" id="">
+        <input class="btn" type="button" value="发送">
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -16,13 +22,51 @@ export default {
 </script>
 <style lang="scss" scoped>
 .log-area {
-  margin-bottom: 60px;
+  margin: 15px 15px 60px 15px;
 }
 .control-area {
   position: fixed;
   bottom: 0px;
-  height: 80px;
+  height: 90px;
   width: 100%;
   background-color: #fff;
+  .transaction {
+    width: 100%;
+    height: 40%;
+    color: #fff;
+    background-color: #07c160;
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .msg {
+    box-sizing: border-box;
+    width: 100%;
+    height: 60%;
+    padding-left: 15px;
+    padding-right: 15px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    .input {
+      flex: 1;
+      border: 0px;
+      border-radius: 6px;
+      height: 60%;
+      margin-right: 10px;
+      padding-left: 5px;
+      padding-right: 5px;
+      background-color: #f0f0f0;
+    }
+    .btn {
+      height: 60%;
+      width: 50px;
+      border: 0px;
+      border-radius: 6px;
+      color: #fff;
+      background-color: #f44;
+    }
+  }
 }
 </style>
